5a0debaff40742673709fbdb417e7cdc0d49e65a
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-28 00:05:03 +0700

    add empty state activity

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -5,7 +5,7 @@ const NavbarComp = () => {
     <>
       <div
         data-cy="header-background"
-        className="h-28 bg-[#16abf8] flex items-center md:px-4 lg:px-44"
+        className="h-28 bg-[#16abf8] flex items-center md:px-4 lg:px-44 shadow-md"
       >
         <p data-cy="header-title" className="text-white font-bold text-2xl ">
           TO DO LIST APP
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ButtonTambah from "../components/ButtonTambah";
+import todoempty from "../images/activity-empty-state.png";
 
 const Dashboard = () => {
   return (
     <>
-      <div>
-        <div>
-          <p>Activity</p>
-          <ButtonTambah />
+      <div className="container md:px-4 lg:px-44">
+        <div className="flex justify-between pt-11">
+          <p data-cy="activity-title" className="font-bold text-4xl">
+            Activity
+          </p>
+          <ButtonTambah data-cy="activity-add" />
+        </div>
+        <div
+          className="flex justify-center pt-14"
+          data-cy="activity-empty-state"
+        >
+          <img
+            src={todoempty}
+            alt="activity-empty-state-img"
+            style={{ width: "500px" }}
+          />
         </div>
       </div>
     </>
