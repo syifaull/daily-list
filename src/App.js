4e011b07b1ed5c7cae995dbe09873491551b502b
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-27 08:33:58 +0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavbarComp from "./components/NavbarComp";
+import Dashboard from "./pages/Dashboard";
+import List from "./pages/List";
 
 function App() {
   return (
-    <div>
-      <NavbarComp />
+    <div className="App">
+      <BrowserRouter>
+        <NavbarComp />
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="/list" element={<List />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
