5e0b4bbee023d637f7eed3c4e9d0557394fc3efd
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-27 08:22:41 +0700

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
+import NavbarComp from "./components/NavbarComp";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hehe</h1>Hello world!
+    <div>
+      <NavbarComp />
     </div>
   );
 }
---
+++ src/components/NavbarComp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavbarComp = () => {
+  return (
+    <>
+      <div className="h-28 bg-[#16abf8] flex items-center px-4 lg:px-44">
+        <p className="text-white font-bold text-2xl ">TO DO LIST APP</p>
+      </div>
+    </>
+  );
+};
+
+export default NavbarComp;
