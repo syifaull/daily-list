8dbb3f3b3969308f7a97331d50ddf7993e0f9b3b
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-29 12:57:38 +0700

    add get single activity and conditional rendering

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,7 +1,7 @@
 import { PencilIcon, TrashIcon } from "@heroicons/react/20/solid";
 import React from "react";
 
-const TodoCard = () => {
+const TodoCard = (props) => {
   return (
     <>
       <div className="px-7 py-7 w-full bg-white rounded-lg shadow-lg flex justify-between mb-3">
@@ -14,12 +14,18 @@ const TodoCard = () => {
           />
           <div className="w-3 h-3 bg-red-600 rounded-full ml-6"></div>
           <label for="remember" className="mx-4">
-            Judul Todo
+            {props.title}
           </label>
-          <PencilIcon className="w-4 h-4 text-[#C4C4C4]" />
+          <PencilIcon
+            className="w-4 h-4 text-[#C4C4C4]"
+            onClick={props.onClickEditToDo}
+          />
         </div>
         <div>
-          <TrashIcon className="w-5 h-5 text-[#888888]" onClick />
+          <TrashIcon
+            className="w-5 h-5 text-[#888888]"
+            onClick={props.onClickDeleteToDo}
+          />
         </div>
       </div>
     </>
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -3,31 +3,68 @@ import {
   ChevronLeftIcon,
   PencilIcon,
 } from "@heroicons/react/20/solid";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import ButtonTambah from "../components/ButtonTambah";
 import TodoCard from "../components/TodoCard";
+import todoempty from "../images/todo-empty-state.png";
 
 const List = () => {
+  const [toDo, setToDo] = useState([]);
+  const [totalToDo, setTotalToDo] = useState([]);
+  const location = useLocation();
+
+  const getToDo = async () => {
+    await axios
+      .get(
+        `https://todo.api.devcode.gethired.id/todo-items?activity_group_id=${location.state.id}`
+      )
+      .then((response) => {
+        setToDo(response.data.data);
+        setTotalToDo(response.data.total);
+      });
+  };
+
+  useEffect(() => {
+    getToDo();
+  }, []);
+
+  const handleDeleteToDo = async () => {
+    await axios
+      .delete("https://todo.api.devcode.gethired.id/todo-items/7068")
+      .then((res) => {
+        getToDo();
+        alert("apakah anda yakin akan menghapusnya?");
+      })
+      .catch((err) => {
+        alert("gagal menghapus");
+      });
+  };
+
   return (
     <>
       <div className="md:px-4 lg:px-44">
         <div className="flex justify-between pt-11">
           <div className="flex">
-            <ChevronLeftIcon className="w-8 h-8" />
+            <Link to="/">
+              <ChevronLeftIcon className="w-8 h-8" />
+            </Link>
+
             <p data-cy="" className="font-bold text-4xl px-6">
               New Activity
             </p>
             <PencilIcon className="w-6 h-6 text-[#C4C4C4]" />
           </div>
           <div className="flex">
-            <div className="border rounded-full w-14 h-14 mr-4">
-              <ArrowsUpDownIcon className="w-6 h-6 text-[#888888]" />
+            <div className="border flex rounded-full w-14 h-14 mr-4">
+              <ArrowsUpDownIcon className="w-6 h-6 text-[#888888] m-auto" />
             </div>
             <ButtonTambah data-cy="" />
           </div>
         </div>
         <div className="activity-container pt-14">
-          {totalActivity === 0 ? (
+          {totalToDo === 0 ? (
             <div className="flex justify-center" data-cy="">
               <img
                 src={todoempty}
@@ -36,9 +73,15 @@ const List = () => {
               />
             </div>
           ) : (
-            <div>
-              <TodoCard />
-            </div>
+            <>
+              {toDo.map((task, index) => {
+                return (
+                  <div>
+                    <TodoCard key={index} title={task.title} />
+                  </div>
+                );
+              })}
+            </>
           )}
         </div>
       </div>
